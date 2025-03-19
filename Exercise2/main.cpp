2de160ba132cd67569ffce0edb2feea97a403855
Beatrice Gnai <s310738@studenti.polito.it>
2025-03-19 12:08:37 +0100

    esercizio finito main.cpp

--- Exercise2/main.cpp
+++ Exercise2/main.cpp
@@ -3,8 +3,10 @@
 #include <vector>
 #include <iomanip>
 
+
 int main()
 {
+	//apertura file input e output
 	std::ifstream file("./data.txt");
 	
 	if(file.fail())
@@ -14,29 +16,6 @@ int main()
 	}
 	 
 	
-	//function that maps points from [1,5] to [-1,2]
-	std::cout << std::fixed << std::setprecision(16); //precisione di 16 decimali
-	std::vector<double> y;   //y:vettore dinamico di double
-	double x;
-
-	while (file >> x)
-	{
-		double y_mappato=(3.0/4.0)*x-(7.0/4.0);
-		y.push_back(y_mappato);
-	}
-	
-	int N = y.size();   //dimensione vettore dinamico
-	std::cout << N << std::endl;
-	
-	//stampa i valori mappati
-	std::cout << "Valori mappati:" << std::endl;
-    for (int i = 0; i <= N; ++i) 
-	{
-        std::cout << "y[" << i << "] = " << y[i] << std::endl;
-    }
-	
-	
-	//apertura file output
 	std::ofstream outputfile("./result.txt");
 	if (outputfile.fail())
 	{
@@ -44,15 +23,34 @@ int main()
 		return 1;
 	}
 	
-	// calcolare la media 
-	double sum = 0;
-	for (int i = 0; i<=N; i++)
+	
+	//funzione lineare da [1,5] a [-1,2]
+	std::cout << std::fixed << std::setprecision(16); //precisione di 16 decimali
+	std::vector<double> y;   //y:vettore dinamico di double
+	double x;				 //x: numero del file
+
+	while (file >> x)
 	{
-		double sum = sum + y[i];
-		double media = sum/(i+1);
-		outputfile << media << std::endl;
+		double y_mappato=(3.0/4.0)*x-(7.0/4.0);
+		y.push_back(y_mappato);    //aggiungo il numero al vettore y
 	}
 	
+	file.close();
 	
-    return 0;
-}
+
+	
+	// calcolo della media progressiva
+	double sum = 0;
+	size_t i = 0;    //dichiaro i come size_t per fare il confronto con la dimensione di y
+	while (i < y.size())
+	{
+		sum = sum + y[i];
+		double media = sum/(i+1);
+		outputfile << media << std::endl;
+		i++;
+	};
+
+	outputfile.close();
+	return 0;
+	
+};
